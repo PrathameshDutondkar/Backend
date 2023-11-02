623591363b9dcbc8517e65794e31faf716fd1a78
Prathamesh Dutondkar <prathamesh.dutondkar@perpetualblock.io>
2023-11-02 16:43:15 +0530

    Update the changes

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
+const port=8080; 
 
 app.post("/register", async (req, resp) => {
   let user = new User(req.body);
@@ -106,4 +107,4 @@ function verifyToken(req,resp,next){
   
 }
 
-app.listen(8080); 
+app.listen(process.env.PORT || port); 
